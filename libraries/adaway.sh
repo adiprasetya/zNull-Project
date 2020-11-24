5c10419442b3e63fb9e6f1a8d9451cb279195dd6
Adi Prasetya <fritziekusumaadiprasetya@gmail.com>
2020-11-24 12:21:44 +0700

    improved code

--- libraries/adaway.sh
+++ libraries/adaway.sh
@@ -6,21 +6,28 @@ echo -e 'RUN WITH ROOT PERMISSIONS !!!'
 exit 1
 fi
 
+name="hosts"
+dir="/system/etc"
+dlDir="/data/adb/modules/znull/adaway"
+source="https://adaway.org/hosts.txt"
+
 if [[ $(which curl) == "" ]]; then
 	echo -e "\nERROR, curl command not found.\n"
 	echo "Trying using wget :)"
 	if [[ $(which wget) == "" ]]; then
 		echo -e "ERROR, wget command not found too.\n" && exit 1
 	else
+		mkdir -p $dlDir
 		echo -e "\nDownloading and Patching AdBlocker by Adaway..."
 		sleep 1
-		wget -q -O /system/etc/hosts https://adaway.org/hosts.txt && echo "Done, AdBlocker has been Planted.\n" || echo "Failed. maybe no internet.\n" 
+		wget -q -O $dlDir/$name $source  &> /dev/null && echo "Done, AdBlocker has been Planted.\n" || echo "Failed. maybe no internet.\n" 
+		mv -f $dlDir/$name $dir/. &> /dev/null
 		sleep 1
 	fi
 else 
 # download hosts file using curl
 	echo -e "\nDownloading and Patching AdBlocker by Adaway..."
 	sleep 1
-	curl -s -o /system/etc/hosts https://adaway.org/hosts.txt && echo "Done, AdBlocker has been Planted.\n" || echo "Failed. maybe no internet.\n" 
+	curl -s -o $dir/$name $source &> /dev/null && echo "Done, AdBlocker has been Planted.\n" || echo "Failed. maybe no internet.\n" 
 	sleep 1
 fi
