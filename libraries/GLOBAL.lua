c8ada562e7c1d639f87cad962978f7aeb799206a
Adi Prasetya <fritziekusumaadiprasetya@gmail.com>
2020-12-03 18:20:24 +0700

    separate global and lite

---
+++ libraries/GLOBAL.lua
@@ -0,0 +1,243 @@
+if gg.getTargetPackage() == "com.tencent.ig" then
+	gg.clearResults()
+else
+    print("ㅤ\n🇲🇨zNull🇲🇨\nYou are not running PUBG MOBILE GLOBAL !!!")
+    os.exit()
+end
+
+gg.setVisible(false)
+
+on = "丨【 ON 】"
+off = "丨【 OFF 】"
+
+HOMEG = 1
+function HOMEG()
+    MENUG =
+        gg.choice(
+        {
+            "➤ WALLHACK & COLOR【 Lobby 】",
+            "➤ MEMORY FREEZE【 Lobby 】",
+            "➤ Magic Bullet【 EveryMatch 】",
+            "➤ Other Cheats...",
+            "☣ ＥＸＩＴ ☣"
+        },
+        nil,
+        ("PUBG MOBILE GLOBAL!")
+    )
+    if MENUG == nil then
+    else
+        if MENUG == 1 then
+            bcwhG()
+        end
+        if MENUG == 2 then
+            bypassG()
+        end
+        if MENUG == 3 then
+            mbL()
+        end
+        if MENUG == 4 then
+            otherG()
+        end
+        if MENUG == 5 then
+            exit()
+        end
+    end
+    PUBGMGH = -1
+end
+
+function otherG()
+    OTHERMENUG =
+        gg.multiChoice(
+        {
+            "➭ Less Recoil [Lobby]",
+            "➭ Magic Bullet [Lobby]",
+            "➭ Aimbot [Lobby]",
+            "➭ Aimlock [Lobby] ",
+            "➭ Small Crosshair [Lobby] ",
+            "➭ Remove Grass [Lobby] ",
+            "➭ Remove Fog [Lobby] ",
+            "☣ Back ☣"
+        },
+        nil,
+        ("PUBG MOBILE GLOBAL!\nThanks to @FakeCez & @NotTeam14!")
+    )
+
+    if OTHERMENUG == nil then
+    else
+        if OTHERMENUG[1] == true then
+            lessG()
+        end
+        if OTHERMENUG[2] == true then
+            mbL()
+            mbG()
+        end
+        if OTHERMENUG[3] == true then
+            aimbotG()
+        end
+        if OTHERMENUG[4] == true then
+            aimlockG()
+        end
+        if OTHERMENUG[5] == true then
+            smallG()
+        end
+        if OTHERMENUG[6] == true then
+            rmgrassG()
+            if OTHERMENUG[7] == true then
+                rmfogG()
+            end
+            if OTHERMENUG[8] == true then
+                HOMEG()
+            end
+        end
+        PUBGMGH = -1
+    end
+
+    -- VALUE
+
+    function bypassG()
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("135682;144387", gg.TYPE_DWORD)
+        gg.refineNumber("135682", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("134658;131586", gg.TYPE_DWORD)
+        gg.refineNumber("134658", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("134914;262403", gg.TYPE_DWORD)
+        gg.refineNumber("134914", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("133378;262403", gg.TYPE_DWORD)
+        gg.refineNumber("133378", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("131330;133634", gg.TYPE_DWORD)
+        gg.refineNumber("131330", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("131842;132098", gg.TYPE_DWORD)
+        gg.refineNumber("131842", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_C_ALLOC)
+        gg.searchNumber("132098", gg.TYPE_DWORD)
+        gg.getResults(50000)
+        gg.editAll("0", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.toast("Memory Freeze ✔")
+    end
+
+    function bcwhG()
+        gg.clearResults()
+        gg.setRanges(gg.REGION_VIDEO)
+        gg.searchNumber("8200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineAddress("E8", -1, gg.TYPE_DWORD, gg.SIGN_EQUAL, 0, -1)
+        gg.getResults(gg.getResultsCount())
+        gg.editAll("6", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.toast("Yellow ✔")
+        gg.clearResults()
+        gg.setRanges(gg.REGION_VIDEO)
+        gg.searchNumber("2.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineAddress("504", -1, gg.TYPE_FLOAT, gg.SIGN_EQUAL, 0, -1)
+        gg.getResults(gg.getResultsCount())
+        gg.editAll("130", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.setRanges(gg.REGION_VIDEO)
+        gg.searchNumber("2.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineAddress("980", -1, gg.TYPE_FLOAT, gg.SIGN_EQUAL, 0, -1)
+        gg.getResults(gg.getResultsCount())
+        gg.editAll("130", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("WallHack ✔")
+    end
+end
+
+function FAKECEZ()
+end
+function setvalue(address, flags, value)
+    FAKECEZ("Modify address value(Address, value type, value to be modified)")
+    local tt = {}
+    tt[1] = {}
+    tt[1].address = address
+    tt[1].flags = flags
+    tt[1].value = value
+    gg.setValues(tt)
+end
+
+function lessG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x130CBF0
+    setvalue(so + py, 16, 0)
+    gg.toast("Less Recoil ✔")
+end
+function smallG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x130D1A8
+    setvalue(so + py, 16, 0)
+    gg.toast("Small Croshair ✔")
+end
+function aimbotG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0xFB4694
+    setvalue(so + py, 16, 0)
+    gg.toast("AimBot ✔")
+end
+function aimlockG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x24A74B0
+    setvalue(so + py, 16, 0)
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x24A74BC
+    setvalue(so + py, 16, 0)
+    gg.toast("AimLock ✔")
+end
+function mbG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x3B65608
+    setvalue(so + py, 16, 40)
+    gg.toast("Magic Bullet ✔")
+end
+
+function rmgrassG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x2475D58
+    setvalue(so + py, 16, 0)
+    gg.toast("Remove Grass ✔")
+end
+function rmfogG()
+    so = gg.getRangesList("libUE4.so")[1].start
+    py = 0x2C344C8
+    setvalue(so + py, 16, 0)
+    gg.toast("Remove Fog ✔")
+end
+
+-- VALUE
+
+	gg.toast("PUBG MOBILE GLOBAL!")
+    gg.clearResults()
+    LobbyAlertG()
+    while true do
+        if gg.isVisible(true) then
+            PUBGMGH = 1
+            gg.setVisible(false)
+        end
+        if PUBGMGH == 1 then
+            HOMEG()
+        end
+    end
---
+++ libraries/LITE.lua
@@ -0,0 +1,265 @@
+if gg.getTargetPackage() == "com.tencent.iglite" then
+	gg.clearResults()
+else
+    print("ㅤ\n🇲🇨zNull🇲🇨\nYou are not running PUBG MOBILE LITE !!!")
+    os.exit()
+end
+
+
+gg.setVisible(false)
+
+on = "丨【 ON 】"
+off = "丨【 OFF 】"
+
+HOMEL = 1
+function HOMEL()
+    MENUL =
+        gg.choice(
+        {
+            "➤ WALLHACK & COLOR【 Lobby 】",
+            "➤ FAST PARACHUTE【 Island 】",
+            "➤ Magic Bullet【 EveryMatch 】",
+            "➤ Other Cheats...",
+            "☣ ＥＸＩＴ ☣"
+        },
+        nil,
+        ("PUBG MOBILE LITE!")
+    )
+    if MENUL == nil then
+    else
+        if MENUL == 1 then
+            bcwhL()
+            fixL()
+        end
+        if MENUL == 2 then
+            fpL()
+        end
+        if MENUL == 3 then
+            mbL()
+        end
+        if MENUL == 4 then
+            otherL()
+        end
+        if MENUL == 5 then
+            exit()
+        end
+    end
+    PUBGMLH = -1
+end
+
+function otherL()
+    OTHERMENUL =
+        gg.multiChoice(
+        {
+            "➭ Antena Head [InGame]",
+            "➭ Magic Bullet [EveryMatch]",
+            "➭ Fast Parachute [Island]",
+            "➭ Speed UAZ [InVehicle] ",
+            "➭ Speed Dacia [InVehicle] ",
+            "☣ Back ☣"
+        },
+        nil,
+        ("PUBG MOBILE LITE!")
+    )
+
+    if OTHERMENUL == nil then
+    else
+        if OTHERMENUL[1] == true then
+            antheadL()
+        end
+        if OTHERMENUL[2] == true then
+            mbL()
+        end
+        if OTHERMENUL[3] == true then
+            fpL()
+        end
+        if OTHERMENUL[4] == true then
+            uazL()
+        end
+        if OTHERMENUL[5] == true then
+            daciaL()
+        end
+        if OTHERMENUL[6] == true then
+            HOMEL()
+        end
+    end
+    PUBGMLH = -1
+end
+
+function bcwhL()
+    -- green
+    gg.setRanges(gg.REGION_VIDEO)
+    gg.searchNumber("8201", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.refineAddress("8", -1, gg.TYPE_DWORD, gg.SIGN_EQUAL, 0, -1)
+    if gg.getResultCount() == 0 then
+        gg.toast("Wallhack and Color is Still Active.")
+        gg.clearResults()
+    else
+        gg.getResults(gg.getResultsCount())
+        gg.editAll("7", gg.TYPE_DWORD)
+        gg.clearResults()
+        -- black for pure it
+        gg.setRanges(gg.REGION_VIDEO)
+        gg.searchNumber("8203", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineAddress("8", -1, gg.TYPE_DWORD, gg.SIGN_EQUAL, 0, -1)
+        gg.getResults(gg.getResultsCount())
+        gg.editAll("8", gg.TYPE_DWORD)
+        gg.clearResults()
+        gg.toast("Green ✔")
+        gg.clearResults()
+        gg.setRanges(gg.REGION_VIDEO)
+        gg.searchNumber("4.8146047e21;3.7615819e-37;2.0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+        gg.refineAddress("0", -1, gg.TYPE_FLOAT, gg.SIGN_EQUAL, 0, -1)
+        gg.getResults(gg.getResultsCount())
+        gg.editAll("130", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("WallHack ✔")
+    end
+end
+
+function antheadL()
+    gg.setVisible(false)
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber(
+        "0.98900693655~0.98900723457;0.14786802232~0.14786840975;1.1920926e-7::9",
+        gg.TYPE_FLOAT,
+        false,
+        gg.SIGN_EQUAL,
+        0,
+        -1
+    )
+    gg.searchNumber("0.98900693655~0.98900723457", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(100)
+    gg.editAll("16000", gg.TYPE_FLOAT)
+    gg.clearResults()
+    gg.toast("Antena Head ✔")
+end
+
+function uazL()
+    gg.clearResults()
+    gg.setRanges(32)
+    gg.searchNumber("0.647058857;0.30000001192;0.94117647409::9", 16, false, 536870912, 0, -1)
+    gg.searchNumber("0.647058857;0.30000001192::5", 16, false, 536870912, 0, -1)
+    gg.searchNumber("0.647058857;0.30000001192::5", 16, false, 536870912, 0, -1)
+    gg.searchNumber("0.647058857;0.30000001192::5", 16, false, 536870912, 0, -1)
+    gg.getResults(50)
+    gg.editAll("100.241295", 16)
+    gg.clearResults()
+    gg.toast("Speed UAZ ✔")
+end
+
+function daciaL()
+    gg.clearResults()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("1000;10;4D;4D;50;5;2;0.03::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.searchNumber("0.03", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.searchNumber("0.03", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.searchNumber("0.03", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(280)
+    gg.editAll("-0.23", gg.TYPE_FLOAT)
+    gg.clearResults()
+    gg.toast("Speed Dacia ✔")
+end
+
+function whfixL()
+    gg.clearResults()
+    gg.setRanges(gg.REGION_VIDEO)
+    gg.searchNumber("4.8146047e21;3.7615819e-37;130", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.refineNumber("130", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+    gg.refineAddress("0", -1, gg.TYPE_FLOAT, gg.SIGN_EQUAL, 0, -1)
+    if gg.getResultCount() == 3 then
+        gg.getResults(1)
+        gg.editAll("2", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("Fixed ✔")
+    elseif gg.getResultCount() == 4 then
+        gg.getResults(2)
+        gg.editAll("2", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("Fixed ✔")
+    elseif gg.getResultCount() == 5 then
+        gg.getResults(3)
+        gg.editAll("2", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("Fixed ✔")
+    elseif gg.getResultCount() == 6 then
+        gg.getResults(4)
+        gg.editAll("2", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("Fixed ✔")
+    elseif gg.getResultCount() == 7 then
+        gg.getResults(5)
+        gg.editAll("2", gg.TYPE_FLOAT)
+        gg.clearResults()
+        gg.toast("Fixed ✔")
+    else
+        gg.toast("Nothing.")
+        gg.clearResults()
+    end
+end
+
+function bypassL()
+    -- bepass sniff
+    gg.clearResults()
+    gg.setRanges(gg.REGION_C_ALLOC)
+    gg.searchNumber('"67109633"', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("0", gg.TYPE_FLOAT)
+    gg.clearResults()
+    gg.setRanges(gg.REGION_C_ALLOC)
+    gg.searchNumber('"67109377"', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(gg.getResultsCount())
+    gg.editAll("0", gg.TYPE_FLOAT)
+    gg.clearResults()
+    gg.toast("Memory Freeze ✔")
+end
+
+function mbL()
+    gg.clearResults()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("h B8 41 00 00 C8 41 00 00 F4 41", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(gg.getResultsCount())
+    if gg.getResultsCount() == 0 then
+        gg.toast("Value Magic Bullet\n[X] Not Found!")
+    else
+        gg.toast("Magic Bullet ✔")
+        gg.editAll("h B8 41 00 00 2F 44 00 00 2F 44", gg.TYPE_BYTE)
+        gg.clearResults()
+    end
+end
+
+function fpL()
+    gg.clearResults()
+    gg.setRanges(32)
+    gg.searchNumber("200;200;1;1::13", 16, false, 536870912, 0, -1)
+    gg.refineNumber("1", 16, false, 536870912, 0, -1)
+    gg.getResultsCount()
+    gg.getResults(21)
+    gg.editAll("2412", 16)
+    gg.clearResults()
+    gg.toast("Fast Parachute ✔")
+end
+
+function LobbyAlertL()
+    gg.setVisible(false)
+    LAL = gg.alert("\n☣ ＬＯＢＢＹ ☣", "☣ YES ☣", "☣ NO ☣")
+    if LAL == 1 then
+        bypassL()
+        bcwhL()
+        mbL()
+    end
+end
+
+	gg.toast("PUBG MOBILE LITE!")
+LobbyAlertL()
+
+    while true do
+        if gg.isVisible(true) then
+            PUBGMLH = 1
+            gg.setVisible(false)
+        end
+        if PUBGMLH == 1 then
+            HOMEL()
+        end
+    end
