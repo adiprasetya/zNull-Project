93d2786deae5665c1fc30b5be6e63b02cf17bbd2
Adi Prasetya <fritziekusumaadiprasetya@gmail.com>
2020-12-30 08:18:54 +0700

    changing mv to cp

--- customize.sh
+++ customize.sh
@@ -18,19 +18,19 @@ echo
 cat $MODPATH/libraries/zNull-Project.txt
 
 if [[ ! -s /data/media/0/Android/znull.conf ]]; then
-	mv -f $MODPATH/libraries/znull.conf /data/media/0/Android/znull.conf
+	cp -f $MODPATH/libraries/znull.conf /data/media/0/Android/znull.conf
 	echo -e "You can change package name on Internal/Android/znull.conf\nor use znull [-s|set].\n"
 else
 	source /data/media/0/Android/znull.conf 2> /dev/null
-	if [[ "$newver" != "true" ]]; then
-		mv -f $MODPATH/libraries/znull.conf /data/media/0/Android/znull.conf
+	if [[ "$version" != "0.5.x" ]]; then
+		cp -f $MODPATH/libraries/znull.conf /data/media/0/Android/znull.conf
 		echo -e "Updated to new Template znull.conf"
 		echo -e "You can configure it again!\n"
 	fi
 fi
 
 
-mv -f $MODPATH/libraries/znull.sh /data/media/0/Android/.
+cp -f $MODPATH/libraries/znull.sh /data/media/0/Android/.
 chmod +x /data/media/0/Android/znull.sh
 
 mv -f $MODPATH/libraries/zNull-SCRIPT.lua /data/media/0/. && echo -e "You can use our script on Internal.\nOur script is OnLy for PUBG MOBILE LITE and GLOBAL!"
